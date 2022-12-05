3b3cb200bababd921456af66da584d8eba985b69
Alexander Lobachev <ss_di@mail.ru>
2022-12-06 00:20:07 +0300

    Задания на 2 кучи

---
+++ 2022_12_06-Task_19-21/p19-21-wrong_play.py
@@ -0,0 +1,43 @@
+def hod(a, b, mh, h=0):
+    if a+b >= 77:
+        if h % 2 == 1:
+            return 1
+        else:
+            return 2
+
+    h += 1
+    if h > mh:
+        return 3
+
+    res = []
+    res.append(hod(a + 1, b, mh, h))
+    res.append(hod(a * 2, b, mh, h))
+    res.append(hod(a, b + 1, mh, h))
+    res.append(hod(a, b * 2, mh, h))
+
+    if h % 2 == 1:
+        if 2 in res:
+            return 2
+        elif 3 in res:
+            return 3
+        else:
+            return 1
+    else:
+        if 2 in res:
+            return 2
+        elif 3 in res:
+            return 3
+        else:
+            return 1
+
+
+print(' ', end='\t')
+for mh in range(1, 6):
+    print(mh, end='\t')
+print()
+
+for b in range(1, 69+1):
+    print(b, end='\t')
+    for mh in range(1, 6):
+        print(hod(7, b, mh), end='\t')
+    print()
