94ac1d55b05df7b84aace0b9e5c7ad1ac0d79941
Alexander Lobachev <ss_di@mail.ru>
2023-02-21 12:59:11 +0300

    z15 на питоне

---
+++ 2023_02_21 - Task 2 and 15 (bit mask)/z15.py
@@ -0,0 +1,11 @@
+def f( x, a ):
+    return (x & 125 != 1) or ((x & 34 == 2) <= (x & a == 0))
+
+for a in range(1, 1000):
+    for x in range(1,1000):
+        if not f(x, a):
+            break
+    else:
+        print( a )
+        break
+    
