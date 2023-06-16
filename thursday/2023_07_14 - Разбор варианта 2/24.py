55d3f56b4e2213da6f53299b1158fc42ac7f43e0
Alexander Lobachev <ss_di@mail.ru>
2023-06-16 19:39:53 +0300

    Update 24.py

--- thursday/2023_07_14 - Разбор варианта 2/24.py
+++ thursday/2023_07_14 - Разбор варианта 2/24.py
@@ -13,14 +13,14 @@ print(data[:2], data[-2:])
 v = data.split("SOLO")
 print(len(v))
 
-d = []
-for w in v:
-    d.append(calc_digit(w))
+#d = []
+#for w in v:
+#    d.append(calc_digit(w))
     
-print(len(d), d[:2], d[-2:], sum(d))
+#print(len(d), d[:2], d[-2:], sum(d))
 
 mx_s = ""
-for i in range(len(v)-5):
+for i in range(len(v)-4):
     s = "SOLO".join(v[i:i+5])
     cnt_d = calc_digit(s)
     if cnt_d >= 5 and len(s) > len(mx_s):
